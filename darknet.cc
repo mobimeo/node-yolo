7e22e3e9475d99f4c73296e8c0798f83f16245c4
Oleksii Rudenko <oleksii.rudenko@moovel.com>
2017-01-30 16:23:19 +0100

    Rename methods

--- darknet.cc
+++ darknet.cc
@@ -1,14 +1,14 @@
 #include <nan.h>
 #include "src/demo.h"
 
-void RunCallback(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  v8::Local<v8::Function> cb = info[1].As<v8::Function>();
-  v8::Local<v8::Object> options = info[0].As<v8::Object>();
+void detect(const Nan::FunctionCallbackInfo<v8::Value>& arguments) {
+  v8::Local<v8::Object> options = arguments[0].As<v8::Object>();
+  v8::Local<v8::Function> cb = arguments[1].As<v8::Function>();
   start_demo(options, cb);
 }
 
 void Init(v8::Local<v8::Object> exports, v8::Local<v8::Object> module) {
-  Nan::SetMethod(module, "exports", RunCallback);
+  Nan::SetMethod(module, "exports", detect);
 }
 
 NODE_MODULE(addon, Init)
